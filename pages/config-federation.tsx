f34b23bafbca008c2d05252d8b3c3c11ccc1c3c9
Gabe Kangas <gabek@real-ity.com>
2021-11-28 15:11:04 -0800

    Add federation info modal. Closes https://github.com/owncast/owncast/issues/1544

--- pages/config-federation.tsx
+++ pages/config-federation.tsx
@@ -1,5 +1,6 @@
-import { Typography } from 'antd';
+import { Typography, Modal, Button } from 'antd';
 import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { TEXTFIELD_TYPE_TEXTAREA, TEXTFIELD_TYPE_URL } from '../components/config/form-textfield';
 import TextFieldWithSubmit from '../components/config/form-textfield-with-submit';
 import ToggleSwitch from '../components/config/form-toggleswitch';
@@ -16,9 +17,55 @@ import {
 } from '../utils/config-constants';
 import { ServerStatusContext } from '../utils/server-status-context';
 
+function FederationInfoModal({ cancelPressed, okPressed }) {
+  return (
+    <Modal
+      width={800}
+      title="Enable Federation"
+      visible
+      onCancel={cancelPressed}
+      footer={<Button onClick={okPressed}>I Understand.</Button>}
+    >
+      <Typography.Title level={3}>What is Federation?</Typography.Title>
+
+      <Typography.Paragraph>
+        Enabling Federation allows your Owncast server to be a part of the greater{' '}
+        <a href="https://en.wikipedia.org/wiki/Fediverse" rel="noopener noreferrer" target="_blank">
+          Fediverse
+        </a>
+        , allowing people to be notified when you go live and for you to send posts from Owncast to
+        your followers. It can also surface actions taken on the Fediverse such as following,
+        sharing or liking to show who is engaging with your stream.
+      </Typography.Paragraph>
+
+      <Typography.Title level={3}>Understand the following</Typography.Title>
+      <Typography.Paragraph>
+        Due to other servers interacting with yours there are some things to keep in mind.
+      </Typography.Paragraph>
+      <ul>
+        <li>You must always host your Owncast server with SSL using a https url.</li>
+        <li>
+          If you ever change your server name or Fediverse username you will be seen as a completely
+          different user on the Fediverse, and the old user will disappear. It is best to never
+          change these once you set them.
+        </li>
+        <li>
+          Turning on Private Federation mode will require you to manually approve each follower.
+        </li>
+      </ul>
+    </Modal>
+  );
+}
+
+FederationInfoModal.propTypes = {
+  cancelPressed: PropTypes.func.isRequired,
+  okPressed: PropTypes.func.isRequired,
+};
+
 export default function ConfigFederation() {
   const { Title } = Typography;
   const [formDataValues, setFormDataValues] = useState(null);
+  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
   const serverStatusData = useContext(ServerStatusContext);
   const { serverConfig } = serverStatusData || {};
 
@@ -31,6 +78,16 @@ export default function ConfigFederation() {
       ...formDataValues,
       [fieldName]: value,
     });
+  };
+
+  const handleEnabledSwitchChange = (value: boolean) => {
+    if (!value) {
+      setFormDataValues({
+        enabled: false,
+      });
+    } else {
+      setIsInfoModalOpen(true);
+    }
   };
 
   // if instanceUrl is empty, we should also turn OFF the `enabled` field of directory.
@@ -48,6 +105,20 @@ export default function ConfigFederation() {
       });
     }
   };
+
+  function federationInfoModalCancelPressed() {
+    setIsInfoModalOpen(false);
+    setFormDataValues({
+      enabled: false,
+    });
+  }
+
+  function federationInfoModalOkPressed() {
+    setIsInfoModalOpen(false);
+    setFormDataValues({
+      enabled: true,
+    });
+  }
 
   useEffect(() => {
     setFormDataValues({
@@ -75,6 +146,7 @@ export default function ConfigFederation() {
       <div className="form-module config-server-details-container">
         <ToggleSwitch
           fieldName="enabled"
+          onChange={handleEnabledSwitchChange}
           {...FIELD_PROPS_ENABLE_FEDERATION}
           checked={formDataValues.enabled}
           disabled={!hasInstanceUrl || !isInstanceUrlSecure}
@@ -114,6 +186,12 @@ export default function ConfigFederation() {
           {...FIELD_PROPS_SHOW_FEDERATION_ENGAGEMENT}
           checked={formDataValues.showEngagement}
         />
+        {isInfoModalOpen && (
+          <FederationInfoModal
+            cancelPressed={federationInfoModalCancelPressed}
+            okPressed={federationInfoModalOkPressed}
+          />
+        )}
         <br />
         <br />
       </div>
